29a39e61f02cb5611695a34a862a14ec13412b00
Teodor168 <teodor16iliev@gmail.com>
2025-10-15 14:23:20 +0300

    TestForEvenOrOdd

---
+++ Izpitvane15.10/Even.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Izpitvane15._10
+{
+    public class Even
+    {
+        public bool IsEven(int number)
+        {
+            if (number % 2 == 0)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ TestForEvenOrOddNumbers/UnitTest1.cs
@@ -0,0 +1,71 @@
+using Izpitvane15._10;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel;
+using System.Diagnostics.CodeAnalysis;
+namespace TestForEvenOrOddNumbers
+{
+    public class Tests
+    {
+        static Even num = new Even();
+
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [TestCase(5)]
+        public void TestIsEvenWithOddNumber(int number)
+        {
+            //arrange
+            //Even num = new Even();
+
+            //act
+            bool result = num.IsEven(number);
+
+            //assert
+            Assert.That(result, Is.False);
+        }
+        [TestCase(56)]
+        public void TestGetEvenWithEvenNumber(int number)
+        {
+            bool res = num.IsEven(number);
+
+            Assert.That(res, Is.True);
+        }
+        [TestCase(-6)]
+        public void TestIsEvenOrOddForNegativeEvenNumber(int number)
+        {
+            bool res = num.IsEven(number);
+
+            Assert.AreEqual(true, res);
+        }
+        [TestCase(-5)]
+        public void TestIsEvenOrOddForNEgativeOddNumber(int number)
+        {
+            bool reult = num.IsEven(number);
+            Assert.AreEqual(false, reult);
+        }
+
+        [Test]
+        [TestCase(0)]
+        [TestCase(8)]
+        [TestCase(-62)]
+        public void TestIsEvenOrOddWithThreeEvenNumbers(int number)
+        {
+            bool res = num.IsEven(number);
+            Assert.AreEqual(true, res);
+        }
+        [Test]
+        [TestCase  (1) ]
+        [TestCase (5)]
+        [TestCase (101)]
+        public void TestIsEvenOrOddWithOddNumbers(int nums)
+        {
+            
+            bool res = num.IsEven(nums);
+            
+            
+            Assert.That(res, Is.False);
+        }
+
+    }
+}
